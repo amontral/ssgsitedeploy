9cd359dd23a281b0831c61baed815922efc250d8
amontral <55558445+amontral@users.noreply.github.com>
2025-04-03 15:03:05 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import App from './App'
-import SBSSAssessment from './SBSSAssessment'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/assessment" element={<SBSSAssessment />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 )
